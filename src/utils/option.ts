4f9786d29bc3c92f81ba4733adec9aa7a7b843c7
Thomas Churchman <thomas@kepow.org>
2019-10-06 21:36:08 +0200

    Add Option methods.

--- src/utils/option.ts
+++ src/utils/option.ts
@@ -11,6 +11,14 @@ export default class Option<T> {
     this._some = some;
   }
 
+  static from<T>(value?: T | null) {
+    if (typeof value === "undefined" || value === null) {
+      return Option.none<T>();
+    } else {
+      return Option.some<T>(value);
+    }
+  }
+
   static some<T>(value: T) {
     return new Option<T>(true, value);
   }
@@ -27,11 +35,38 @@ export default class Option<T> {
     return !this._some;
   }
 
+  /**
+   * Get the value inside the option if it is Some. Throws an error otherwise.
+   */
   unwrap(): T {
     if (this._some) {
       return this._value as T;
     } else {
       throw new Error("Tried to unwrap None");
+    }
+  }
+
+  /**
+   * Get the value inside the option if it is Some. Return the alternative
+   * otherwise.
+   */
+  unwrapOr(alternative: T): T {
+    if (this._some) {
+      return this._value as T;
+    } else {
+      return alternative;
+    }
+  }
+
+  /**
+   * Get the value inside the option if it is Some. Otherwise, call the function
+   * to generate an alternative and return that.
+   */
+  unwrapOrElse(fnAlternative: () => T): T {
+    if (this._some) {
+      return this._value as T;
+    } else {
+      return fnAlternative();
     }
   }
 
