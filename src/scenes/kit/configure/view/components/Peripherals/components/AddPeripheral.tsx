AstroPlant/astroplant-frontend-web

commit d1098714046f3acb851d24b157f6bb9baf814fbe

Extract peripheral definition card to a component.

src/scenes/kit/configure/view/components/Peripherals/components/AddPeripheral.tsx
@@ -22,6 +22,8 @@ import { AuthConfiguration } from "utils/api";
 import { JSONSchema6 } from "json-schema";
 import ApiForm from "Components/ApiForm";
 
+import PeripheralDefinitionCard from "Components/PeripheralDefinitionCard";
+
 type State = {
   open: boolean;
   done: boolean;
@@ -142,22 +144,10 @@ class AddPeripheral extends React.Component<PInner, State> {
             {Object.keys(this.props.peripheralDefinitions).map(id => {
               const def = this.props.peripheralDefinitions[id];
               return (
-                <Card
-                  key={id}
-                  color="blue"
-                  link
-                  onClick={() => this.selectPeripheralDefinition(def)}
-                >
-                  <Card.Content>
-                    <Card.Header>{def.name}</Card.Header>
-                    {def.description && (
-                      <Card.Description>{def.description}</Card.Description>
-                    )}
-                    <Card.Meta>
-                      {def.brand} - {def.model}
-                    </Card.Meta>
-                  </Card.Content>
-                </Card>
+                <PeripheralDefinitionCard
+                  key={def.id}
+                  peripheralDefinition={def}
+                />
               );
             })}
           </Card.Group>

src/Components/PeripheralDefinitionCard.tsx
@@ -1,0 +1,24 @@
+import React from "react";
+import { Card, CardProps } from "semantic-ui-react";
+import { PeripheralDefinition } from "astroplant-api";
+
+export type Props = CardProps & {
+  peripheralDefinition: PeripheralDefinition;
+};
+
+export default (props: Props) => {
+  const { peripheralDefinition: def, ...rest } = props;
+  return (
+    <Card color="blue" { ...rest }>
+      <Card.Content>
+        <Card.Header>{def.name}</Card.Header>
+        {def.description && (
+          <Card.Description>{def.description}</Card.Description>
+        )}
+        <Card.Meta>
+          {def.brand} - {def.model}
+        </Card.Meta>
+      </Card.Content>
+    </Card>
+  );
+};

src/scenes/kit/configure/view/components/Peripherals/components/ViewEditPeripheral.tsx
@@ -27,6 +27,8 @@ import ApiForm from "Components/ApiForm";
 import ApiButton from "Components/ApiButton";
 import RjsfForm from "rjsf-theme-semantic-ui";
 
+import PeripheralDefinitionCard from "Components/PeripheralDefinitionCard";
+
 type State = {
   editing: boolean;
 };
@@ -122,17 +124,7 @@ class ViewEditPeripheral extends React.Component<PInner, State> {
         <Label attached="top">Peripheral #{peripheral.id}</Label>
         <Header>{peripheral.name}</Header>
         <p>Identifier: #{peripheral.id}</p>
-        <Card color="blue" fluid>
-          <Card.Content>
-            <Card.Header>{def.name}</Card.Header>
-            {def.description && (
-              <Card.Description>{def.description}</Card.Description>
-            )}
-            <Card.Meta>
-              {def.brand} - {def.model}
-            </Card.Meta>
-          </Card.Content>
-        </Card>
+        <PeripheralDefinitionCard peripheralDefinition={def} fluid />
         {this.state.editing ? (
           <>
             <PeripheralForm

commit e078a34032fd3b7edc205ca42f94dc4a3cf02dd6

Fix regression introduced in d1098714.

src/scenes/kit/configure/view/components/Peripherals/components/AddPeripheral.tsx
@@ -147,6 +147,7 @@ class AddPeripheral extends React.Component<PInner, State> {
                 <PeripheralDefinitionCard
                   key={def.id}
                   peripheralDefinition={def}
+                  onClick={() => this.selectPeripheralDefinition(def)}
                 />
               );
             })}