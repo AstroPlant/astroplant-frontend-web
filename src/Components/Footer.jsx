0bacfacc7de9e27004d9df879c82addb2ec0b151
Thomas Churchman <thomas@kepow.org>
2019-11-21 23:18:15 +0100

    Add version statement Footer.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import packageJson from "../../package.json";
 
 class Footer extends Component {
   render() {
@@ -8,6 +9,9 @@ class Footer extends Component {
         <Container>
           <p>
             Copyright © AstroPlant 2018. Source code available on <a href="https://github.com/astroplant">GitHub</a>.
+          </p>
+          <p>
+          Version: {packageJson.version}
           </p>
         </Container>
       </footer>
