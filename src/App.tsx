f68675e83b32376296e1d40f9dd7e9490bd6f15c
Thomas Churchman <thomas@kepow.org>
2020-01-29 13:24:27 +0100

    Add Map scene.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { withTranslation, WithTranslation } from "react-i18next";
 import { RootState } from "types";
 import Option from "./utils/option";
 import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
@@ -19,12 +20,25 @@ import SignUp from "./pages/SignUp";
 import Me from "./pages/Me";
 import CreateKit from "./pages/CreateKit";
 import Kit from "./scenes/kit";
+import Map from "./scenes/map";
 
 type Props = WithTranslation & {
   displayName: Option<string>;
 };
 
 class App extends Component<Props> {
+  componentDidMount() {
+    const L = require("leaflet");
+
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    });
+  }
+
   render() {
     const { t } = this.props;
     return (
@@ -37,7 +51,12 @@ class App extends Component<Props> {
               to: "/home",
               key: "home"
             },
-            //{ as: NavLink, content: t("common.map"), to: "/map", key: "map" }
+            {
+              as: NavLink,
+              content: t("common.map"),
+              to: "/map",
+              key: "map"
+            }
           ]}
           rightItems={
             this.props.displayName.isSome()
@@ -73,10 +92,8 @@ class App extends Component<Props> {
             <Route exact path="/terms-and-conditions">
               <PageLoader page="TermsAndConditions" />
             </Route>
-            <Route path="/map">
-              <PageLoader page="Map" />
-            </Route>
             <Route path="/test" component={TestComponent} />
+            <Route path="/map" component={Map} />
             <Route path="/log-in">
               <LogIn />
             </Route>
