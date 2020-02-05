dc6290eafec35d528d2faad83b90028c976d7132
Thomas Churchman <thomas@kepow.org>
2020-02-05 17:26:14 +0100

    Show time at which raw measurement was made.

--- src/scenes/kit/overview/components/RawMeasurement.tsx
+++ src/scenes/kit/overview/components/RawMeasurement.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardProps, Statistic, Loader } from "semantic-ui-react";
+import Moment from "react-moment";
 import { RawMeasurement } from "modules/kit/reducer";
 import { Peripheral, PeripheralDefinition, QuantityType } from "astroplant-api";
 import Option from "utils/option";
@@ -23,19 +24,37 @@ export default (props: Props) => {
     <Card color="blue" {...rest}>
       <Card.Content>
         <Card.Header>{quantityType.physicalQuantity}</Card.Header>
-        <Card.Description textAlign='center'>
+        <Card.Description textAlign="center">
           <Statistic>
             <Statistic.Value>
               {rawMeasurement
                 .map(m => <span>{Math.round(m.value * 100) / 100}</span>)
-                .unwrapOr(<Loader active inline='centered' />)}
+                .unwrapOr(<Loader active inline="centered" />)}
             </Statistic.Value>
             <Statistic.Label>
               {quantityType.physicalUnitSymbol || quantityType.physicalUnit}
             </Statistic.Label>
           </Statistic>
         </Card.Description>
-        <Card.Meta>Measured by {peripheral.name}</Card.Meta>
+        {rawMeasurement
+          .map(m => {
+            return (
+              <>
+                <Card.Meta>
+                  Measured{" "}
+                  <Moment fromNow interval={15000}>
+                    {m.datetime}
+                  </Moment>
+                </Card.Meta>
+              </>
+            );
+          })
+          .unwrapOrNull()}
+      </Card.Content>
+      <Card.Content extra>
+        <Card.Meta>
+          <code>{peripheral.name}</code>
+        </Card.Meta>
       </Card.Content>
     </Card>
   );
