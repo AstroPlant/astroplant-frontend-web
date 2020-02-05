67b0cd2248f75dfca917f27ec4b87ece8a705276
Thomas Churchman <thomas@kepow.org>
2020-02-05 20:49:18 +0100

    Make test usable.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./store";
+import * as actions from "./modules/generic/actions";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+store.dispatch(actions.pageInitializationSuccess());
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
