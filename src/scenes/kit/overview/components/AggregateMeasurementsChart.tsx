1b3cc673c2aa8f7bdda72ba1c3a327d455e66fdc
Thomas Churchman <thomas@kepow.org>
2020-02-10 14:55:21 +0100

    Improve aggregate charts.
    Add tooltip.
    Use i18n.

--- src/scenes/kit/overview/components/AggregateMeasurementsChart.tsx
+++ src/scenes/kit/overview/components/AggregateMeasurementsChart.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { withTranslation, WithTranslation } from "react-i18next";
 import { Card, CardProps } from "semantic-ui-react";
 import {
   ResponsiveContainer,
   ComposedChart,
+  Tooltip,
   Area,
   XAxis,
   YAxis
@@ -19,19 +21,21 @@ export type Measurements = {
   };
 };
 
-export type Props = CardProps & {
-  peripheral: Peripheral;
-  peripheralDefinition: PeripheralDefinition;
-  quantityType: QuantityType;
-  measurements: Option<Array<Measurements>>;
-};
+export type Props = CardProps &
+  WithTranslation & {
+    peripheral: Peripheral;
+    peripheralDefinition: PeripheralDefinition;
+    quantityType: QuantityType;
+    measurements: Option<Array<Measurements>>;
+  };
 
-export default (props: Props) => {
+const AggregateMeasurementsChart = (props: Props) => {
   const {
     peripheral,
     peripheralDefinition,
     quantityType,
     measurements,
+    t,
     ...rest
   } = props;
   return (
@@ -41,10 +45,16 @@ export default (props: Props) => {
         <Card.Description textAlign="center">
           <ResponsiveContainer height={300} width="100%">
             <ComposedChart data={measurements.unwrapOr([])}>
+              <Tooltip
+                formatter={(value: any, name: any) =>
+                  parseFloat(value.toPrecision(4))
+                }
+                labelFormatter={(time: any) => moment(time).format("L LTS")}
+              />
               <XAxis
                 dataKey="datetimeStart"
                 tickFormatter={tick => moment(tick).calendar()}
-                minTickGap={25}
+                minTickGap={40}
               />
               <YAxis
                 yAxisId="left"
@@ -56,25 +66,21 @@ export default (props: Props) => {
                 tickFormatter={tick => tick.toPrecision(4)}
                 padding={{ top: 50, bottom: 0 }}
               />
-              {/*
-              <YAxis
-               yAxisId="right"
-               orientation="right"
-               label="Count"
-               domain={["dataMin", "dataMax"]}
-               allowDecimals={false}
+              <Area
+                yAxisId="left"
+                dataKey="values.minimum"
+                name={t("kit.aggregateMeasurements.minimum") as string}
               />
-              */}
-              <Area yAxisId="left" dataKey="values.minimum" />
-              <Area yAxisId="left" dataKey="values.average" />
-              <Area yAxisId="left" dataKey="values.maximum" />
-              {/*
-              <Line
-                yAxisId="right"
-                dot={false}
-                dataKey="values.count"
+              <Area
+                yAxisId="left"
+                dataKey="values.average"
+                name={t("kit.aggregateMeasurements.average") as string}
               />
-              */}
+              <Area
+                yAxisId="left"
+                dataKey="values.maximum"
+                name={t("kit.aggregateMeasurements.maximum") as string}
+              />
             </ComposedChart>
           </ResponsiveContainer>
           {measurements.isNone() && (
@@ -92,12 +98,18 @@ export default (props: Props) => {
                 opacity: 0.6
               }}
             >
-              <h2 style={{ textAlign: "center" }}>No measurements</h2>
+              <h2 style={{ textAlign: "center" }}>
+                {t("kit.aggregateMeasurements.noMeasurements")}
+              </h2>
             </div>
           )}
         </Card.Description>
-        <Card.Meta>Measured by {peripheral.name}</Card.Meta>
+        <Card.Meta>
+          {t("kit.aggregateMeasurements.measuredBy", { peripheral })}
+        </Card.Meta>
       </Card.Content>
     </Card>
   );
 };
+
+export default withTranslation()(AggregateMeasurementsChart);
