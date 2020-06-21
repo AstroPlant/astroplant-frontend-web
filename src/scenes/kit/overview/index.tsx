63ebd87f9bb8c091573e5099a8c147e7e1431ba2
Thomas Churchman <thomas@kepow.org>
2020-06-21 21:11:44 +0200

    Make overview tab navigable.

--- src/scenes/kit/overview/index.tsx
+++ src/scenes/kit/overview/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { RouteComponentProps } from "react-router";
+import { RouteComponentProps, Switch, Route } from "react-router";
+import { NavLink } from "react-router-dom";
 import { withTranslation, WithTranslation } from "react-i18next";
 import { Container, Loader, Tab } from "semantic-ui-react";
 import { KitState } from "modules/kit/reducer";
@@ -16,6 +17,7 @@ export type Props = WithTranslation &
   };
 
 function KitOverview(props: Props) {
+  const baseUrl = props.match.url;
   const { t, kitState } = props;
   let activeConfiguration = null;
   for (const configuration of Object.values(
@@ -29,31 +31,56 @@ function KitOverview(props: Props) {
   if (activeConfiguration !== null) {
     const panes = [
       {
-        menuItem: "Measurements",
-        render: () => (
-          <Tab.Pane>
-            <>
-              <h2>Now</h2>
-              <RawMeasurements kitState={kitState} />
-              <h2>Past</h2>
-              <AggregateMeasurements kitState={kitState} />
-            </>
-          </Tab.Pane>
+        menuItem: {
+          as: NavLink,
+          content: "Measurements",
+          to: `${baseUrl}`,
+          exact: true,
+          key: "measurements",
+        },
+        pane: (
+          <Route
+            path={`${baseUrl}`}
+            exact
+            render={() => (
+              <Tab.Pane>
+                <>
+                  <h2>Now</h2>
+                  <RawMeasurements kitState={kitState} />
+                  <h2>Past</h2>
+                  <AggregateMeasurements kitState={kitState} />
+                </>
+              </Tab.Pane>
+            )}
+          />
         ),
       },
       {
-        menuItem: "Media",
-        render: () => (
-          <Tab.Pane>
-            <Media kitState={kitState} />
-          </Tab.Pane>
+        menuItem: {
+          as: NavLink,
+          content: "Media",
+          to: `${baseUrl}/media`,
+          exact: true,
+          key: "media",
+        },
+        pane: (
+          <Route
+            path={`${baseUrl}/media`}
+            render={() => (
+              <Tab.Pane>
+                <Media kitState={kitState} />
+              </Tab.Pane>
+            )}
+          />
         ),
       },
     ];
 
     return (
       <Container>
-        <Tab panes={panes} />
+        <Switch>
+          <Tab panes={panes} renderActiveOnly={false} activeIndex={-1} />
+        </Switch>
       </Container>
     );
   } else if (kitState.configurations.isNone()) {
