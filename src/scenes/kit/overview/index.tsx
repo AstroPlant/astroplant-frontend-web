4589dad2a642ad5b88d2a1336f7cb3d55585f9f7
Thomas Churchman <thomas@kepow.org>
2020-06-21 15:55:16 +0200

    Add media display UI.

--- src/scenes/kit/overview/index.tsx
+++ src/scenes/kit/overview/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
 import { withTranslation, WithTranslation } from "react-i18next";
-import { Container, Loader } from "semantic-ui-react";
+import { Container, Loader, Header, Tab } from "semantic-ui-react";
 import { KitState } from "modules/kit/reducer";
 
 import RawMeasurements from "./components/RawMeasurements";
 import AggregateMeasurements from "./components/AggregateMeasurements";
+import Media from "./components/Media";
 
 type Params = { kitSerial: string };
 
@@ -26,12 +27,33 @@ function KitOverview(props: Props) {
   }
 
   if (activeConfiguration !== null) {
+    const panes = [
+      {
+        menuItem: "Measurements",
+        render: () => (
+          <Tab.Pane>
+            <>
+              <h2>Now</h2>
+              <RawMeasurements kitState={kitState} />
+              <h2>Past</h2>
+              <AggregateMeasurements kitState={kitState} />
+            </>
+          </Tab.Pane>
+        ),
+      },
+      {
+        menuItem: "Media",
+        render: () => (
+          <Tab.Pane>
+            <Media kitState={kitState} />
+          </Tab.Pane>
+        ),
+      },
+    ];
+
     return (
       <Container>
-        <h2>Current measurements</h2>
-        <RawMeasurements kitState={kitState} />
-        <h2>Past measurements</h2>
-        <AggregateMeasurements kitState={kitState} />
+        <Tab panes={panes} />
       </Container>
     );
   } else if (kitState.configurations.isNone()) {
@@ -48,7 +70,7 @@ function KitOverview(props: Props) {
             fontWeight: "bolder",
             opacity: 0.6,
             textAlign: "center",
-            marginTop: "1rem"
+            marginTop: "1rem",
           }}
         >
           <h2>{t("kit.noActiveConfiguration")}</h2>
