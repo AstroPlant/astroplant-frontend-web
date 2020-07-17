ec6c334e2a7fce651893c191bc832f87d591ef6f
Thomas Churchman <thomas@kepow.org>
2020-07-17 15:48:12 +0200

    Only update kit memberships if changed
    This prevents the object reference from changing unnecessarily, thereby
    preventing unnecessary rerenders.

--- src/modules/me/reducer.ts
+++ src/modules/me/reducer.ts
@@ -1,4 +1,6 @@
 import { createReducer, ActionType } from "typesafe-actions";
+import produce from "immer";
+import isEqual from "lodash/isEqual";
 import * as actions from "./actions";
 import Option from "../../utils/option";
 import { FullUser } from "astroplant-api";
@@ -11,14 +13,13 @@ export interface KitMembership {
 export interface MeState {
   details: Option<FullUser>;
   loadingKitMemberships: boolean;
-  kitMemberships: {[serial: string]: KitMembership};
+  kitMemberships: { [serial: string]: KitMembership };
 }
-
 
 const initial: MeState = {
   details: Option.none(),
   loadingKitMemberships: false,
-  kitMemberships: {}
+  kitMemberships: {},
 };
 
 export type MeAction = ActionType<typeof actions>;
@@ -28,11 +29,18 @@ export default createReducer<MeState, MeAction>(initial)
     return { ...state, details: Option.some(action.payload) };
   })
   .handleAction(actions.setKitMemberships, (state, action) => {
-    let kitMemberships: {[serial: string]: KitMembership} = {};
-    for (const { kit, accessConfigure, accessSuper } of action.payload) {
-      kitMemberships[kit.serial] = { accessConfigure, accessSuper };
-    }
-    return { ...state, loadingKitMemberships: false, kitMemberships };
+    return produce(state, (draft) => {
+      for (const { kit, accessConfigure, accessSuper } of action.payload) {
+        const membership = { accessConfigure, accessSuper };
+        if (
+          !(kit.serial in state.kitMemberships) ||
+          !isEqual(membership, state.kitMemberships[kit.serial])
+        ) {
+          draft.kitMemberships[kit.serial] = membership;
+        }
+        draft.loadingKitMemberships = false;
+      }
+    });
   })
   .handleAction(actions.loadingKitMemberships, (state) => {
     return { ...state, loadingKitMemberships: true };
