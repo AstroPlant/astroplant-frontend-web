73158a755c96810b2572c88369f518f015afeffc
Thomas Churchman <thomas@kepow.org>
2020-07-20 01:40:32 +0200

    Only log state changes in non-production builds

--- src/store.ts
+++ src/store.ts
@@ -4,11 +4,15 @@ import { persistStore } from "redux-persist";
 import { rootReducer, rootEpic } from "./root";
 
 const logger = (store: any) => (next: any) => (action: any) => {
-  console.group(action.type);
-  console.info("dispatching", action);
+  if (process.env.NODE_ENV !== "production") {
+    console.group(action.type);
+    console.info("dispatching", action);
+  }
   let result = next(action);
-  console.log("next state", store.getState());
-  console.groupEnd();
+  if (process.env.NODE_ENV !== "production") {
+    console.log("next state", store.getState());
+    console.groupEnd();
+  }
   return result;
 };
 
