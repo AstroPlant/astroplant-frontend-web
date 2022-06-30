aa379143f6dabe90e87f0606ac91e6c919a1754f
Thomas Churchman <thomas@kepow.org>
2022-06-30 23:33:27 +0200

    Flake: simplify

--- flake.nix
+++ flake.nix
@@ -8,25 +8,26 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     let
       version = "0.1.0-alpha.1";
-      overlay = self: super: {
-        astroplant-frontend-modules = self.callPackage ./nix/modules.nix { inherit version; };
-        astroplant-frontend = self.callPackage ./nix/build.nix { inherit version; };
-      };
     in
-    rec {
-      overlays."astroplant-frontend" = overlay;
-      overlays.default = overlays."astroplant-frontend";
+    {
+      overlays.default = final: prev: {
+        astroplant-frontend-modules = final.callPackage ./nix/modules.nix { inherit version; };
+        astroplant-frontend = final.callPackage ./nix/build.nix { inherit version; };
+      };
     } //
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
       in
       {
         packageExprs.astroplant-frontend = { apiUrl, websocketUrl }:
           pkgs.astroplant-frontend.override { inherit apiUrl websocketUrl; };
         packages.astroplant-frontend = pkgs.astroplant-frontend;
         defaultPackage = self.packages.${system}.astroplant-frontend;
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             bashInteractive
             nodejs-18_x
