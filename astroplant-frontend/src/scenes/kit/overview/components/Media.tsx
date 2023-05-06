AstroPlant/astroplant-frontend-web

commit 909a139c915b4fc6e84a1f33c46d4a4ddc7ffb09

error popup, kit restart alert, kit serial

astroplant-frontend/src/scenes/kit/overview/components/Media.tsx
@@ -6,6 +6,7 @@ import { KitState, KitConfigurationState } from "~/modules/kit/reducer";
 import { selectors as peripheralDefinitionsSelectors } from "~/modules/peripheral-definition/reducer";
 import { KitsApi, schemas } from "~/api";
 import { configuration, rateLimit } from "~/utils/api";
+import { tap } from "rxjs/operators";
 
 export type Props = {
   kitState: KitState;
@@ -57,14 +58,19 @@ export default function Media(props: Props) {
   useEffect(() => {
     if (displayMedia) {
       (async () => {
-        const kitsApi = new KitsApi(configuration);
-        const response = await kitsApi
-          .getMediaContent({ mediaId: displayMedia.id })
-          .pipe(rateLimit)
-          .toPromise();
+        try {
+          const kitsApi = new KitsApi(configuration);
+          const response = await kitsApi
+            .getMediaContent({ mediaId: displayMedia.id + "AIAA" })
+            .pipe(rateLimit)
+            .toPromise();
 
-        const url = URL.createObjectURL(response.content);
-        setDisplayUrl(url);
+          const url = URL.createObjectURL(response.content);
+          setDisplayUrl(url);
+        } catch (error) {
+          alert(error);
+          setDisplayMedia(null);
+        }
       })();
     }
   }, [displayMedia]);

astroplant-frontend/src/scenes/kit/access/components/KitSerial.tsx
@@ -1,0 +1,44 @@
+import React from "react";
+import { compose } from "recompose";
+import { withTranslation, WithTranslation } from "react-i18next";
+import { Card, Icon, Input } from "semantic-ui-react";
+
+import { KitsApi, Kit } from "astroplant-api";
+
+import ApiButton from "../../../../Components/ApiButton";
+import { AuthConfiguration } from "../../../../utils/api";
+
+type State = {
+  password?: string;
+};
+
+export type Props = {
+  kit: Kit;
+};
+
+type PInner = WithTranslation & Props;
+
+const Button = ApiButton<any>();
+
+class KitSerial extends React.Component<PInner, State> {
+  state: State = {};
+
+  render() {
+    const { t, kit } = this.props;
+
+    const kitName = kit.name || "Unnamed";
+
+    return (
+      <Card color="orange" centered raised fluid>
+        <Card.Content>
+          <Card.Header>Kit serial</Card.Header>
+          <Card.Description>
+            {kit.serial}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+export default compose<PInner, Props>(withTranslation())(KitSerial);

astroplant-frontend/src/scenes/kit/access/index.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from "react-router";
 import { Container } from "semantic-ui-react";
 
 import { KitContext } from "../contexts";
+import KitSerial from "./components/KitSerial";
 import ResetPassword from "./components/ResetPassword";
 
 export type Props = RouteComponentProps<{}>;
@@ -12,7 +13,8 @@ export default function KitConfigure(_props: Props) {
 
   return (
     <Container text>
-        <ResetPassword kit={kit} />
+      <KitSerial kit={kit} />
+      <ResetPassword kit={kit} />
     </Container>
   );
 }

astroplant-frontend/src/scenes/kit/configure/view/components/ActivateDeactivate.tsx
@@ -39,6 +39,7 @@ class ActivateDeactivate extends React.Component<PInner> {
       serial: kit.serial,
       configuration: response,
     });
+    alert("Configuration updated. Make sure to restart the kit for the configuration to activate.");
   }
 
   send() {

commit 070a3ccc523db18def3451f0cd7efe637299fa28

Fix media id issue introduced in 909a139c915b4fc6e84a1f33c46d4a4ddc7ffb09

astroplant-frontend/src/scenes/kit/overview/components/Media.tsx
@@ -61,7 +61,7 @@ export default function Media(props: Props) {
         try {
           const kitsApi = new KitsApi(configuration);
           const response = await kitsApi
-            .getMediaContent({ mediaId: displayMedia.id + "AIAA" })
+            .getMediaContent({ mediaId: displayMedia.id })
             .pipe(rateLimit)
             .toPromise();
 