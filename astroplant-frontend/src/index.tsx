19de2f13dcd9b86d3557a800afc136534f8f1b68
Thomas Churchman <thomas@kepow.org>
2023-05-06 22:10:09 +0200

    Remove (unused) service worker

--- astroplant-frontend/src/index.tsx
+++ astroplant-frontend/src/index.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import * as actions from "./modules/generic/actions";
-import * as serviceWorker from "./serviceWorker";
 import { store } from "./store";
 import en from "./translations/en.json";
 
@@ -37,8 +36,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
