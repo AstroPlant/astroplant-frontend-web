4ddfccd744c9d172246829f3d779a0c262d37604
Thomas Churchman <thomas@kepow.org>
2023-07-02 11:31:45 +0200

    Dev build: exclude workspace dep from pre-bundling
    Prior to this, environment variables to set the api root URIs would not
    propagate to astroplant-api.

--- astroplant-frontend/vite.config.ts
+++ astroplant-frontend/vite.config.ts
@@ -1,26 +1,27 @@
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // This changes the out put dir from dist to build
-    // comment this out if that isn't relevant for your project
-    resolve: {
-        preserveSymlinks: true,
-    },
-    define: {
-        'process.env': {}
-    },
-    build: {
-        outDir: 'build',
-    },
-    plugins: [
-        reactRefresh(),
-        tsconfigPaths()
-    ],
-    test: {
-        environment: "jsdom"
-    }
-})
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  resolve: {
+    preserveSymlinks: true,
+  },
+  define: {
+    "process.env": {},
+  },
+  build: {
+    outDir: "build",
+  },
+  plugins: [reactRefresh(), tsconfigPaths()],
+  optimizeDeps: {
+    // Exclude workspace dependency astroplant-api from bundling
+    exclude: ["astroplant-api"],
+  },
+  test: {
+    environment: "jsdom",
+  },
+});
