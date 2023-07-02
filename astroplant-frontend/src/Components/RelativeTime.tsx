324602a91486ec53f9bf62ca1f6b4bde14929eb3
Thomas Churchman <thomas@kepow.org>
2023-07-02 11:50:37 +0200

    RelativeTime: update on prop change and simplify

--- astroplant-frontend/src/Components/RelativeTime.tsx
+++ astroplant-frontend/src/Components/RelativeTime.tsx
@@ -3,24 +3,17 @@ import { DateTime } from "luxon";
 
 const RelativeTime = (props: { to: DateTime }) => {
   const [relativeTime, setRelativeTime] = useState(props.to.toRelative());
-  const [intervalId, setIntervalId] = useState<null | NodeJS.Timer>(null);
 
   useEffect(() => {
-    let mounted = true;
-    setIntervalId(
-      setInterval(() => {
-        if (mounted) {
-          setRelativeTime(props.to.toRelative());
-        }
-      }, 10000)
-    );
+    setRelativeTime(props.to.toRelative());
+    const intervalId = setInterval(() => {
+      setRelativeTime(props.to.toRelative());
+    }, 1000);
+
     return function cleanup() {
-      mounted = false;
-      if (intervalId !== null) {
-        clearInterval(intervalId);
-      }
+      clearInterval(intervalId);
     };
-  }, []);
+  }, [props.to]);
 
   return <>{relativeTime}</>;
 };
