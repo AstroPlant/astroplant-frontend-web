558f5d33d3b267bae96f18e1cd10d3924faa8c63
Thomas Churchman <thomas@kepow.org>
2023-07-08 12:21:00 +0200

    Simplify KitSerial component

--- astroplant-frontend/src/scenes/kit/access/components/KitSerial.tsx
+++ astroplant-frontend/src/scenes/kit/access/components/KitSerial.tsx
@@ -1,35 +1,19 @@
 import React from "react";
-import { compose } from "recompose";
-import { withTranslation, WithTranslation } from "react-i18next";
 import { Card } from "semantic-ui-react";
 
 import { Kit } from "astroplant-api";
-
-type State = {
-  password?: string;
-};
 
 export type Props = {
   kit: Kit;
 };
 
-type PInner = WithTranslation & Props;
-
-class KitSerial extends React.Component<PInner, State> {
-  state: State = {};
-
-  render() {
-    const { kit } = this.props;
-
-    return (
-      <Card color="orange" centered raised fluid>
-        <Card.Content>
-          <Card.Header>Kit serial</Card.Header>
-          <Card.Description>{kit.serial}</Card.Description>
-        </Card.Content>
-      </Card>
-    );
-  }
+export default function KitSerial({ kit }: Props) {
+  return (
+    <Card color="orange" centered raised fluid>
+      <Card.Content>
+        <Card.Header>Kit serial</Card.Header>
+        <Card.Description>{kit.serial}</Card.Description>
+      </Card.Content>
+    </Card>
+  );
 }
-
-export default compose<PInner, Props>(withTranslation())(KitSerial);
