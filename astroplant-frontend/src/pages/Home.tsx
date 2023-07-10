c31e52db10d24c45f17cb4872e7b215fd7e55d07
Thomas Churchman <thomas@kepow.org>
2023-07-10 16:35:48 +0200

    Make Home a functional component

--- astroplant-frontend/src/pages/Home.tsx
+++ astroplant-frontend/src/pages/Home.tsx
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
-import { withTranslation, WithTranslation } from "react-i18next";
+import React from "react";
 import { Container } from "semantic-ui-react";
 
 import HeadTitle from "../Components/HeadTitle";
+import { useTranslation } from "react-i18next";
 
-class Home extends Component<WithTranslation> {
-  render() {
-    const { t } = this.props;
+export default function Home() {
+  const { t } = useTranslation();
 
-    return (
-      <>
-        <HeadTitle main={t("home.header")} secondary={t("home.subheader")} />
-        <Container text style={{ marginTop: "1em" }}>
-          <h2>{t("home.content.title")}</h2>
+  return (
+    <>
+      <HeadTitle main={t("home.header")} secondary={t("home.subheader")} />
+      <Container text style={{ marginTop: "1em" }}>
+        <h2>{t("home.content.title")}</h2>
 
-          <p>{t("home.content.body1")}</p>
-          <p>{t("home.content.body2")}</p>
-          <p>{t("home.content.body3")}</p>
-        </Container>
+        <p>{t("home.content.body1")}</p>
+        <p>{t("home.content.body2")}</p>
+        <p>{t("home.content.body3")}</p>
+      </Container>
 
-        {/*
+      {/*
         <Divider />
 
         <Container>
@@ -58,9 +57,6 @@ class Home extends Component<WithTranslation> {
           </Segment>
         </Container>
        */}
-      </>
-    );
-  }
+    </>
+  );
 }
-
-export default withTranslation()(Home);
