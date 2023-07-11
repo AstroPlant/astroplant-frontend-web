30decdb7dfeac385e54e7d3fc9fe32b02f8403b8
Thomas Churchman <thomas@kepow.org>
2023-07-11 20:14:36 +0200

    Make ResetPassword a functional component

--- astroplant-frontend/src/scenes/kit/access/components/ResetPassword.tsx
+++ astroplant-frontend/src/scenes/kit/access/components/ResetPassword.tsx
@@ -1,73 +1,59 @@
-import React from "react";
-import compose from "~/utils/compose";
-import { withTranslation, WithTranslation } from "react-i18next";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Card, Icon, Input } from "semantic-ui-react";
 
 import ApiButton from "../../../../Components/ApiButton";
 import { api, Response, schemas } from "~/api";
 
-type State = {
-  password?: string;
-};
-
 export type Props = {
   kit: schemas["Kit"];
 };
 
-type PInner = WithTranslation & Props;
-
 const Button = ApiButton<any>();
 
-class ResetPassword extends React.Component<PInner, State> {
-  state: State = {};
+export default function ResetPassword({ kit }: Props) {
+  const { t } = useTranslation();
+  const [password, setPassword] = useState<string | null>(null);
 
-  onResponse(response: Response<string>) {
-    this.setState({ password: response.data });
-  }
+  const onResponse = (response: Response<string>) => {
+    setPassword(response.data);
+  };
 
-  send() {
-    const { kit } = this.props;
-
+  const send = () => {
     return api.resetPassword({
       kitSerial: kit.serial,
     });
-  }
+  };
 
-  render() {
-    const { t, kit } = this.props;
+  const kitName = kit.name || "Unnamed";
 
-    const kitName = kit.name || "Unnamed";
-
-    return (
-      <Card color="orange" centered raised fluid>
-        <Card.Content>
-          <Card.Header>Kit password</Card.Header>
-          <Card.Description>
-            {this.state.password ? (
-              <Input
-                fluid
-                icon={<Icon name="lock" inverted circular link />}
-                value={this.state.password || ""}
-                readOnly
-              />
-            ) : (
-              <Button
-                send={this.send.bind(this)}
-                onResponse={this.onResponse.bind(this)}
-                label={t("kitAccess.resetPassword")}
-                buttonProps={{ negative: true }}
-                confirm={() => ({
-                  content: t("kitAccess.resetPasswordConfirm", {
-                    kitName,
-                  }),
-                })}
-              />
-            )}
-          </Card.Description>
-        </Card.Content>
-      </Card>
-    );
-  }
+  return (
+    <Card color="orange" centered raised fluid>
+      <Card.Content>
+        <Card.Header>Kit password</Card.Header>
+        <Card.Description>
+          {password ? (
+            <Input
+              fluid
+              icon={<Icon name="lock" inverted circular link />}
+              value={password || ""}
+              readOnly
+            />
+          ) : (
+            <Button
+              send={send}
+              onResponse={onResponse}
+              label={t("kitAccess.resetPassword")}
+              buttonProps={{ negative: true }}
+              confirm={() => ({
+                content: t("kitAccess.resetPasswordConfirm", {
+                  kitName,
+                }),
+              })}
+            />
+          )}
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  );
 }
-
-export default compose<PInner, Props>(withTranslation())(ResetPassword);
