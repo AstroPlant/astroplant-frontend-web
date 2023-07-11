74700862f934b9964368147766968da48bdc37ac
Thomas Churchman <thomas@kepow.org>
2023-07-11 14:16:03 +0200

    Refactor Kit epics to use new api

--- astroplant-frontend/src/modules/kit/epic.ts
+++ astroplant-frontend/src/modules/kit/epic.ts
@@ -6,39 +6,36 @@ import {
   map,
   filter,
   catchError,
-  takeUntil
+  takeUntil,
 } from "rxjs/operators";
 import * as actions from "./actions";
 import * as meActions from "../me/actions";
 import * as genericActions from "../generic/actions";
 
-import { KitsApi } from "astroplant-api";
-import { AuthConfiguration, requestWrapper } from "~/utils/api";
 import { PDNotFound, PDForbidden } from "../../problems";
+import { api } from "~/api";
 
-const addKitFromKitMemberships: Epic = (actions$, state$) =>
+const addKitFromKitMemberships: Epic = (actions$, _state$) =>
   actions$.pipe(
     filter(meActions.setKitMemberships.match),
-    map(action => action.payload),
-    switchMap(kitMemberships =>
-      from(kitMemberships.map(kitMembership => kitMembership.kit))
+    map((action) => action.payload),
+    switchMap((kitMemberships) =>
+      from(kitMemberships.map((kitMembership) => kitMembership.kit))
     ),
     map(actions.addKit)
   );
 
-const fetchKit: Epic = (actions$, state$) =>
+const fetchKit: Epic = (actions$, _state$) =>
   actions$.pipe(
     filter(actions.fetchKit.match),
-    map(action => action.payload.serial),
-    map(kitSerial => {
-      const api = new KitsApi(AuthConfiguration.Instance);
+    map((action) => action.payload.serial),
+    map((kitSerial) => {
       return { serial: kitSerial, req: api.showKitBySerial({ kitSerial }) };
     }),
     mergeMap(({ serial, req }) =>
       req.pipe(
-        requestWrapper(),
-        map(kit => actions.addKit(kit)),
-        catchError(err => {
+        map((kit) => actions.addKit(kit.data)),
+        catchError((err) => {
           if (PDNotFound.is(err.response)) {
             return of(actions.notFound({ serial }));
           } else if (PDForbidden.is(err.response)) {
@@ -51,11 +48,11 @@ const fetchKit: Epic = (actions$, state$) =>
     )
   );
 
-const kitWatching: Epic = (actions$, state$) =>
+const kitWatching: Epic = (actions$, _state$) =>
   actions$.pipe(
     filter(actions.startWatching.match),
-    map(action => action.payload.serial),
-    mergeMap(serial =>
+    map((action) => action.payload.serial),
+    mergeMap((serial) =>
       timer(0, 60 * 1000).pipe(
         mergeMap(() =>
           concat(of(actions.kitConfigurationsRequest({ serial })))
@@ -63,27 +60,27 @@ const kitWatching: Epic = (actions$, state$) =>
         takeUntil(
           actions$.pipe(
             filter(actions.stopWatching.match),
-            filter(action => action.payload.serial === serial)
+            filter((action) => action.payload.serial === serial)
           )
         )
       )
     )
   );
 
-const kitConfigurationsRequest: Epic = (actions$, state$) =>
+const kitConfigurationsRequest: Epic = (actions$, _state$) =>
   actions$.pipe(
     filter(actions.kitConfigurationsRequest.match),
-    map(action => action.payload.serial),
-    map(kitSerial => {
-      const api = new KitsApi(AuthConfiguration.Instance);
+    map((action) => action.payload.serial),
+    map((kitSerial) => {
       return { serial: kitSerial, req: api.listConfigurations({ kitSerial }) };
     }),
     mergeMap(({ serial, req }) =>
       req.pipe(
-        map(configurations =>
+        map((response) => response.data),
+        map((configurations) =>
           actions.kitConfigurationsSuccess({ serial, configurations })
         ),
-        catchError(err => of(genericActions.setApiConnectionFailed(true)))
+        catchError((_err) => of(genericActions.setApiConnectionFailed(true)))
       )
     )
   );
