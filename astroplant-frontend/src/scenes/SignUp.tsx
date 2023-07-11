1ca573885ba01fda523eaf6b85cc9d43f4ab730b
Thomas Churchman <thomas@kepow.org>
2023-07-11 20:59:13 +0200

    Make SignUp a functional component

--- astroplant-frontend/src/scenes/SignUp.tsx
+++ astroplant-frontend/src/scenes/SignUp.tsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { withTranslation, WithTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import {
   Container,
   Segment,
   Header,
   Icon,
-  Transition
+  Transition,
 } from "semantic-ui-react";
 import { JSONSchema7 } from "json-schema";
 import ApiForm from "~/Components/ApiForm";
@@ -15,18 +15,14 @@ import HeadTitle from "~/Components/HeadTitle";
 
 import { UsersApi } from "astroplant-api";
 
-type State = {
-  done: boolean;
-};
-
 const SignUpForm = ApiForm();
 
-class SignUpPage extends Component<WithTranslation, State> {
-  state = {
-    done: false
-  };
+export default function SignUpPage() {
+  const { t } = useTranslation();
+  const [done, setDone] = useState(false);
 
-  validate(formData: any, errors: any) {
+  const validate = (formData: any, errors: any) => {
+    console.warn("VALIDATE");
     if (formData.password !== formData.passwordRepeat) {
       errors.passwordRepeat.addError("passwords do not match");
     }
@@ -35,89 +31,83 @@ class SignUpPage extends Component<WithTranslation, State> {
       errors.termsOfService.addError("you must agree to the Terms of Service");
     }
     return errors;
-  }
+  };
 
-  onResponse() {
-    this.setState({ done: true });
-  }
+  const onResponse = () => {
+    setDone(true);
+  };
 
-  send(data: any) {
+  const send = (data: any) => {
     const api = new UsersApi();
     return api.createUser({
-      newUser: data
+      newUser: data,
     });
-  }
+  };
 
-  render() {
-    const { t } = this.props;
-
-    const schema: JSONSchema7 = {
-      type: "object",
-      title: "Account details",
-      required: ["username", "password", "passwordRepeat", "emailAddress"],
-      properties: {
-        username: { type: "string", title: t("common.username") },
-        password: { type: "string", title: t("common.password") },
-        passwordRepeat: { type: "string", title: t("account.passwordRepeat") },
-        emailAddress: { type: "string", title: t("common.emailAddress") },
-        termsOfService: {
-          type: "boolean",
-          title: t("signUp.readAgreeTermsOfService")
-        }
-      }
-    };
-
-    const uiSchema = {
-      password: {
-        "ui:widget": "password"
+  const schema: JSONSchema7 = {
+    type: "object",
+    title: "Account details",
+    required: ["username", "password", "passwordRepeat", "emailAddress"],
+    properties: {
+      username: { type: "string", title: t("common.username") },
+      password: { type: "string", title: t("common.password") },
+      passwordRepeat: { type: "string", title: t("account.passwordRepeat") },
+      emailAddress: { type: "string", title: t("common.emailAddress") },
+      termsOfService: {
+        type: "boolean",
+        title: t("signUp.readAgreeTermsOfService"),
       },
-      passwordRepeat: {
-        "ui:widget": "password"
-      },
-      emailAddress: {
-        // e-mail widget seems to be missing in @rjsf/semantic-ui
-        // "ui:widget": "email"
-      }
-    };
+    },
+  };
 
-    return (
-      <>
-        <HeadTitle
-          main="Create an account"
-          secondary="Create an account to connect to the AstroPlant world."
-        />
-        <Container text style={{ marginTop: "1em" }}>
-          <Segment padded>
-            {this.state.done ? (
-              <>
-                <Header size="huge" icon textAlign="center">
-                  <Transition animation="drop" duration={450} transitionOnMount>
-                    <Icon name="check" circular />
-                  </Transition>
-                  <Header.Content>Success!</Header.Content>
-                </Header>
-                <Container textAlign="center">
-                  <p>
-                    Your account has been created.{" "}
-                    <Link to="/log-in">You can now log in.</Link>
-                  </p>
-                </Container>
-              </>
-            ) : (
-              <SignUpForm
-                schema={schema}
-                uiSchema={uiSchema}
-                validate={this.validate}
-                send={this.send}
-                onResponse={this.onResponse.bind(this)}
-                submitLabel={t("signUp.createAccount")}
-              />
-            )}
-          </Segment>
-        </Container>
-      </>
-    );
-  }
+  const uiSchema = {
+    password: {
+      "ui:widget": "password",
+    },
+    passwordRepeat: {
+      "ui:widget": "password",
+    },
+    emailAddress: {
+      // e-mail widget seems to be missing in @rjsf/semantic-ui
+      // "ui:widget": "email"
+    },
+  };
+
+  return (
+    <>
+      <HeadTitle
+        main="Create an account"
+        secondary="Create an account to connect to the AstroPlant world."
+      />
+      <Container text style={{ marginTop: "1em" }}>
+        <Segment padded>
+          {done ? (
+            <>
+              <Header size="huge" icon textAlign="center">
+                <Transition animation="drop" duration={450} transitionOnMount>
+                  <Icon name="check" circular />
+                </Transition>
+                <Header.Content>Success!</Header.Content>
+              </Header>
+              <Container textAlign="center">
+                <p>
+                  Your account has been created.{" "}
+                  <Link to="/log-in">You can now log in.</Link>
+                </p>
+              </Container>
+            </>
+          ) : (
+            <SignUpForm
+              schema={schema}
+              uiSchema={uiSchema}
+              validate={validate}
+              send={send}
+              onResponse={onResponse}
+              submitLabel={t("signUp.createAccount")}
+            />
+          )}
+        </Segment>
+      </Container>
+    </>
+  );
 }
-
-export default withTranslation()(SignUpPage);
