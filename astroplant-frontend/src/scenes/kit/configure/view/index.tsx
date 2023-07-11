8b9d87628c536ee8a962ec7404485b727308afb8
Thomas Churchman <thomas@kepow.org>
2023-07-11 21:34:26 +0200

    Simplify ViewConfiguration component

--- astroplant-frontend/src/scenes/kit/configure/view/index.tsx
+++ astroplant-frontend/src/scenes/kit/configure/view/index.tsx
@@ -1,64 +1,47 @@
 import React, { useContext } from "react";
-import { RouteComponentProps } from "react-router";
-import { withTranslation, WithTranslation } from "react-i18next";
-import compose from "~/utils/compose";
+import { useParams } from "react-router";
+import { useTranslation } from "react-i18next";
 
 import { Container, Segment, Header, Divider } from "semantic-ui-react";
-import { KitConfigurationState } from "~/modules/kit/reducer";
 
-import Option from "~/utils/option";
-
-import { KitContext, ConfigurationsContext } from "../../contexts";
-
+import { KitContext, ConfigurationsContext } from "~/scenes/kit/contexts";
 import Description from "./components/Description";
 import Rules from "./components/Rules";
 import ActivateDeactivate from "./components/ActivateDeactivate";
 import Peripherals from "./components/Peripherals";
 
-type Params = { configurationId: string };
-
-export type Props = RouteComponentProps<Params>;
-type InternalProps = Props & WithTranslation;
-
-function ViewConfiguration(props: InternalProps) {
-  const { t } = props;
-  const { configurationId } = props.match.params;
+export default function ViewConfiguration() {
+  const { t } = useTranslation();
+  const { configurationId } = useParams<{ configurationId: string }>();
 
   const kit = useContext(KitContext);
   const configurations = useContext(ConfigurationsContext);
 
-  const configuration: Option<KitConfigurationState> = Option.from(configurations![configurationId]);
+  const configuration = configurations[configurationId];
 
-  if (configuration.isNone()) {
+  if (configuration === undefined) {
     return <div>Configuration not found.</div>;
   } else {
     return (
       <Container>
         <Segment raised>
           <Header>Description</Header>
-          <Description kit={kit} configuration={configuration.unwrap()} />
+          <Description kit={kit} configuration={configuration} />
         </Segment>
         <Container textAlign="right">
-          <ActivateDeactivate
-            kit={kit}
-            configuration={configuration.unwrap()}
-          />
+          <ActivateDeactivate kit={kit} configuration={configuration} />
         </Container>
         <Divider />
         <Container>
           <Header>{t("control.header")}</Header>
-          <Rules kit={kit} configuration={configuration.unwrap()} />
+          <Rules kit={kit} configuration={configuration} />
         </Container>
         <Divider />
         <Container>
           <Header>Peripherals</Header>
-          <Peripherals kit={kit} configuration={configuration.unwrap()} />
+          <Peripherals kit={kit} configuration={configuration} />
         </Container>
       </Container>
     );
   }
 }
-
-export default compose<InternalProps, Props>(withTranslation())(
-  ViewConfiguration
-);
