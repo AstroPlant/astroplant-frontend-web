714fd89006ad9aa48dc52fed6abc3e35d5711aa2
Thomas Churchman <thomas@kepow.org>
2023-07-16 14:19:36 +0200

    Remove unused InfoBox component

--- astroplant-frontend/src/pages/Home.tsx
+++ astroplant-frontend/src/pages/Home.tsx
@@ -17,46 +17,6 @@ export default function Home() {
         <p>{t("home.content.body2")}</p>
         <p>{t("home.content.body3")}</p>
       </Container>
-
-      {/*
-        <Divider />
-
-        <Container>
-          <Segment placeholder>
-            <Grid columns={3} stackable textAlign="center">
-              <Grid.Row divided verticalAlign="middle">
-                <Grid.Column>
-                  <InfoBox
-                    icon="map marker alternate"
-                    header={t("home.buttons.see.header")}
-                    body={t("home.buttons.see.body")}
-                    button={t("home.buttons.see.button")}
-                    route="/home"
-                  />
-                </Grid.Column>
-                <Grid.Column>
-                  <InfoBox
-                    icon="pagelines"
-                    header={t("home.buttons.learn.header")}
-                    body={t("home.buttons.learn.body")}
-                    button={t("home.buttons.learn.button")}
-                    route="/home"
-                  />
-                </Grid.Column>
-                <Grid.Column>
-                  <InfoBox
-                    icon="dashboard"
-                    header={t("home.buttons.analyze.header")}
-                    body={t("home.buttons.analyze.body")}
-                    button={t("home.buttons.analyze.button")}
-                    route="/analyze"
-                  />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment>
-        </Container>
-       */}
     </>
   );
 }
