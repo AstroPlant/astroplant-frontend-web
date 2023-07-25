269a121747b496126ed168258e90e6ac03b43e69
Thomas Churchman <thomas@kepow.org>
2023-07-25 15:56:39 +0200

    Make ConnectionStatus a functional component
    And fix name

--- astroplant-frontend/src/Components/ConnectionStatus.tsx
+++ astroplant-frontend/src/Components/ConnectionStatus.tsx
@@ -1,37 +1,27 @@
 import React from "react";
-import { connect } from "react-redux";
-import { withTranslation, WithTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { Container, Message } from "semantic-ui-react";
-import { RootState } from "~/types";
+import { useAppSelector } from "~/hooks";
 
-type Props = WithTranslation & {
-  apiConnectionFailed: boolean;
-};
+export default function ConnectionStatus() {
+  const { t } = useTranslation();
+  const apiConnectionFailed = useAppSelector(
+    (state) => state.generic.apiConnectionFailed,
+  );
 
-class Notifications extends React.Component<Props> {
-  render() {
-    const { t } = this.props;
-
-    if (this.props.apiConnectionFailed) {
-      return (
-        <div style={{ position: "fixed", top: "3em", left: "3em" }}>
-          <Container text>
-            <Message
-              error={true}
-              header={t("permanentConnectionIssue.title")}
-              content={t("permanentConnectionIssue.body")}
-            />
-          </Container>
-        </div>
-      );
-    } else {
-      return <></>;
-    }
+  if (apiConnectionFailed) {
+    return (
+      <div style={{ position: "fixed", top: "3em", left: "3em" }}>
+        <Container text>
+          <Message
+            error={true}
+            header={t("permanentConnectionIssue.title")}
+            content={t("permanentConnectionIssue.body")}
+          />
+        </Container>
+      </div>
+    );
+  } else {
+    return <></>;
   }
 }
-
-const mapStateToProps = (state: RootState) => {
-  return { apiConnectionFailed: state.generic.apiConnectionFailed };
-};
-
-export default withTranslation()(connect(mapStateToProps)(Notifications));
