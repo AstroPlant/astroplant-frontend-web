2f0f2ad6bcff2c245fd9e5192b73d212d26f9cda
Thomas Churchman <thomas@kepow.org>
2023-07-25 12:00:06 +0200

    Make CoordinateField a functional component

--- astroplant-frontend/src/rjsf-theme/CoordinateField/index.tsx
+++ astroplant-frontend/src/rjsf-theme/CoordinateField/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import compose from "~/utils/compose";
-import { withTranslation, WithTranslation } from "react-i18next";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import { FieldProps } from "@rjsf/utils";
 import {
@@ -12,13 +11,6 @@ import {
 } from "react-leaflet";
 import { Button } from "semantic-ui-react";
 import { MarkerIcon } from "~/Components/MarkerIcon";
-
-type State = {
-  latitude: number | null;
-  longitude: number | null;
-};
-
-type InnerProps = FieldProps & WithTranslation;
 
 function MapListener({
   click,
@@ -36,79 +28,67 @@ function MapListener({
   return null;
 }
 
-class CoordinateField extends React.Component<InnerProps, State> {
-  state: State = {
-    latitude: null,
-    longitude: null,
-  };
+export default function CoordinateField(props: FieldProps) {
+  const { onChange, formData } = props;
+  const { t } = useTranslation();
 
-  constructor(props: InnerProps) {
-    super(props);
-    this.state = {
-      latitude: (props.formData || {}).latitude || null,
-      longitude: (props.formData || {}).longitude || null,
-    };
-  }
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
 
-  componentDidMount() {
-    if (this.state.latitude === null || this.state.longitude === null) {
-      this.props.onChange(undefined);
+  useEffect(() => {
+    setLatitude(formData?.latitude);
+    setLongitude(formData?.longitude);
+  }, [formData]);
+
+  useEffect(() => {
+    if (latitude === null || longitude === null) {
+      onChange(undefined);
+    } else {
+      onChange({ latitude, longitude });
     }
-  }
+  }, [onChange, latitude, longitude]);
 
-  onChange(latLng: { lat: number | null; lng: number | null }) {
-    this.setState({ latitude: latLng.lat, longitude: latLng.lng }, () => {
-      if (this.state.latitude === null || this.state.longitude === null) {
-        this.props.onChange(undefined);
-      } else {
-        this.props.onChange(this.state);
-      }
-    });
-  }
-
-  render() {
-    const { t } = this.props;
-    const { latitude, longitude } = this.state;
-    return (
-      <div>
-        <p>
-          <strong>Kit location</strong>
-        </p>
-        <p>Select a position on the map to set as your kit's location</p>
-        <MapContainer center={[35, 0]} zoom={2} style={{ height: "45em" }}>
-          <MapListener click={(latLng) => this.onChange(latLng)} />
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {typeof latitude === "number" && typeof longitude === "number" && (
-            <Marker icon={MarkerIcon} position={[latitude!, longitude!]}>
-              <Popup>
-                <h3>{t("common.position")}</h3>
-                <p>
-                  {t("common.latitude")}: {latitude.toFixed(4)}
-                  <br />
-                  {t("common.longitude")}: {longitude.toFixed(4)}
-                </p>
-                <p>
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      this.onChange({ lat: null, lng: null });
-                    }}
-                  >
-                    {t("common.remove")}
-                  </Button>
-                </p>
-              </Popup>
-            </Marker>
-          )}
-        </MapContainer>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>
+        <strong>Kit location</strong>
+      </p>
+      <p>Select a position on the map to set as your kit's location</p>
+      <MapContainer center={[35, 0]} zoom={2} style={{ height: "45em" }}>
+        <MapListener
+          click={(latLng) => {
+            setLatitude(latLng.lat);
+            setLongitude(latLng.lng);
+          }}
+        />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {typeof latitude === "number" && typeof longitude === "number" && (
+          <Marker icon={MarkerIcon} position={[latitude!, longitude!]}>
+            <Popup>
+              <h3>{t("common.position")}</h3>
+              <p>
+                {t("common.latitude")}: {latitude.toFixed(4)}
+                <br />
+                {t("common.longitude")}: {longitude.toFixed(4)}
+              </p>
+              <p>
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setLatitude(null);
+                    setLongitude(null);
+                  }}
+                >
+                  {t("common.remove")}
+                </Button>
+              </p>
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer>
+    </div>
+  );
 }
-
-export default compose<InnerProps, FieldProps>(withTranslation())(
-  CoordinateField,
-);
