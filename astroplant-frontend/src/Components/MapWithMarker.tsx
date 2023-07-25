3954a2427d4c5b45186402d516d999077dfdc5a8
Thomas Churchman <thomas@kepow.org>
2023-07-25 16:22:59 +0200

    Make MapWithMarker a functional component

--- astroplant-frontend/src/Components/MapWithMarker.tsx
+++ astroplant-frontend/src/Components/MapWithMarker.tsx
@@ -6,28 +6,26 @@ type Props = {
   location: { latitude: number; longitude: number };
 };
 
-export default class MapWithMarker extends React.Component<
-  React.PropsWithChildren<Props>
-> {
-  render() {
-    const { location } = this.props;
-    return (
-      <MapContainer
-        center={[location.latitude, location.longitude]}
-        zoom={10}
-        style={{ height: "45em" }}
+export default function MapWithMarker({
+  location,
+  children,
+}: React.PropsWithChildren<Props>) {
+  return (
+    <MapContainer
+      center={[location.latitude, location.longitude]}
+      zoom={10}
+      style={{ height: "45em" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker
+        icon={MarkerIcon}
+        position={[location.latitude, location.longitude]}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker
-          icon={MarkerIcon}
-          position={[location.latitude, location.longitude]}
-        >
-          {this.props.children && <Popup>{this.props.children}</Popup>}
-        </Marker>
-      </MapContainer>
-    );
-  }
+        {children && <Popup>{children}</Popup>}
+      </Marker>
+    </MapContainer>
+  );
 }
