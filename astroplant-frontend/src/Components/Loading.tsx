b632c6da7c58a6601507a9fbeba10eb5ecf3b97b
Thomas Churchman <thomas@kepow.org>
2023-08-15 22:21:51 +0200

    Add Spinner component

--- astroplant-frontend/src/Components/Loading.tsx
+++ astroplant-frontend/src/Components/Loading.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Segment, Placeholder } from "semantic-ui-react";
 
+import style from "./Loading.module.css";
+
 export default function Loading() {
   return (
     <Container>
@@ -15,5 +17,19 @@ export default function Loading() {
         </Placeholder>
       </Segment>
     </Container>
+  );
+}
+
+export type SpinnerProps = {
+  size?: string | number;
+};
+
+export function Spinner({ size = "1em" }) {
+  return (
+    <div
+      style={{ fontSize: size }}
+      className={style.spinner}
+      aria-label="Loading"
+    />
   );
 }
