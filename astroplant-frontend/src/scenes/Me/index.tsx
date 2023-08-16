d80ecbc1e2a9176fbe40a9a785992b308bb8868d
Thomas Churchman <thomas@kepow.org>
2023-08-16 12:00:50 +0200

    Make Me a functional component

--- astroplant-frontend/src/scenes/Me/index.tsx
+++ astroplant-frontend/src/scenes/Me/index.tsx
@@ -21,87 +21,85 @@ type Props = WithTranslation &
     kitStates: RootState["kit"]["kits"];
   };
 
-class Me extends Component<Props> {
-  render() {
-    const { t, me } = this.props;
+function Me(props: Props) {
+  const { t, me } = props;
 
-    return (
-      <>
-        <HeadTitle
-          main={t("me.header", {
-            displayName: this.props.me.displayName,
-          })}
-        />
-        <Container text style={{ marginTop: "1em" }}>
-          <Header size="large">Your kits</Header>
-          {Object.keys(this.props.kitMemberships).length > 0 ||
-          this.props.loadingKitMemberships ? (
-            <>
-              <p>
-                <Link to="/create-kit">Create another.</Link>
-              </p>
-              {Object.keys(this.props.kitMemberships).length > 0 && (
-                <Card.Group>
-                  {Object.keys(this.props.kitMemberships).map((serial) => {
-                    const kitState = this.props.kitStates.entities[serial];
-                    return (
-                      <Card
-                        fluid
-                        key={serial}
-                        color="orange"
-                        as={Link}
-                        to={`/kit/${serial}`}
-                      >
-                        <Card.Content>
-                          <Image floated="right" size="mini">
-                            <Gravatar identifier={serial} />
-                          </Image>
-                          <Card.Header>
-                            {kitState
-                              ? (kitState.details && kitState.details.name) ||
-                                t("kit.unnamed")
-                              : "Loading"}
-                          </Card.Header>
-                          <Card.Meta>Serial: {serial}</Card.Meta>
-                        </Card.Content>
-                      </Card>
-                    );
-                  })}
-                </Card.Group>
-              )}
-              {this.props.loadingKitMemberships && <PlaceholderSegment />}
-            </>
-          ) : (
-            <div>
-              You have no kits yet.{" "}
-              <Link to="/create-kit">You can create one!</Link>
-            </div>
-          )}
-          <Divider />
-          <Card centered raised>
-            <Image>
-              <Gravatar
-                identifier={
-                  me.useEmailAddressForGravatar
-                    ? me.emailAddress
-                    : me.gravatarAlternative
-                }
-              />
-            </Image>
-            <Card.Content>
-              <Card.Header>{this.props.me.displayName}</Card.Header>
-              <Card.Meta>{this.props.me.emailAddress}</Card.Meta>
-            </Card.Content>
-            <Card.Content extra>
-              {t("me.content.usernameLabel", {
-                username: this.props.me.username,
-              })}
-            </Card.Content>
-          </Card>
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <HeadTitle
+        main={t("me.header", {
+          displayName: props.me.displayName,
+        })}
+      />
+      <Container text style={{ marginTop: "1em" }}>
+        <Header size="large">Your kits</Header>
+        {Object.keys(props.kitMemberships).length > 0 ||
+        props.loadingKitMemberships ? (
+          <>
+            <p>
+              <Link to="/create-kit">Create another.</Link>
+            </p>
+            {Object.keys(props.kitMemberships).length > 0 && (
+              <Card.Group>
+                {Object.keys(props.kitMemberships).map((serial) => {
+                  const kitState = props.kitStates.entities[serial];
+                  return (
+                    <Card
+                      fluid
+                      key={serial}
+                      color="orange"
+                      as={Link}
+                      to={`/kit/${serial}`}
+                    >
+                      <Card.Content>
+                        <Image floated="right" size="mini">
+                          <Gravatar identifier={serial} />
+                        </Image>
+                        <Card.Header>
+                          {kitState
+                            ? (kitState.details && kitState.details.name) ||
+                              t("kit.unnamed")
+                            : "Loading"}
+                        </Card.Header>
+                        <Card.Meta>Serial: {serial}</Card.Meta>
+                      </Card.Content>
+                    </Card>
+                  );
+                })}
+              </Card.Group>
+            )}
+            {props.loadingKitMemberships && <PlaceholderSegment />}
+          </>
+        ) : (
+          <div>
+            You have no kits yet.{" "}
+            <Link to="/create-kit">You can create one!</Link>
+          </div>
+        )}
+        <Divider />
+        <Card centered raised>
+          <Image>
+            <Gravatar
+              identifier={
+                me.useEmailAddressForGravatar
+                  ? me.emailAddress
+                  : me.gravatarAlternative
+              }
+            />
+          </Image>
+          <Card.Content>
+            <Card.Header>{props.me.displayName}</Card.Header>
+            <Card.Meta>{props.me.emailAddress}</Card.Meta>
+          </Card.Content>
+          <Card.Content extra>
+            {t("me.content.usernameLabel", {
+              username: props.me.username,
+            })}
+          </Card.Content>
+        </Card>
+      </Container>
+    </>
+  );
 }
 
 const mapStateToProps = (state: RootState) => {
