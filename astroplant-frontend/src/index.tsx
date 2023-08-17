fe0e0092b08e3df92f78beb8e420d03c1d91a7a5
Thomas Churchman <thomas@kepow.org>
2023-08-17 15:29:37 +0200

    Use React.createRoot

--- astroplant-frontend/src/index.tsx
+++ astroplant-frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import i18n from "i18next";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { initReactI18next } from "react-i18next";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
@@ -28,11 +28,16 @@ window.addEventListener("load", () => {
   store.dispatch(actions.pageLoadSuccess());
 });
 
-ReactDOM.render(
+const container = document.getElementById("root");
+if (container === null) {
+  throw new Error("could not find element to render into");
+}
+
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root"),
 );
