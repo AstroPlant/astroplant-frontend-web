7e0b6233715e2f22fd7ef9e990ced6fc96452cdc
Thomas Churchman <thomas@kepow.org>
2023-08-31 17:18:55 +0200

    Add some notes

--- astroplant-frontend/src/permissions.ts
+++ astroplant-frontend/src/permissions.ts
@@ -24,12 +24,16 @@ export const NO_PERMISSIONS: KitPermissions = {
   rpcPeripheralCommandLock: false,
 };
 
+// Might be better if the API returns the permissions, so we don't have to
+// calculate them here.
 export function kitPermissionsFromMembership(
   membership: KitMembership | null,
 ): KitPermissions {
   let accessConfigure = membership?.accessConfigure ?? false;
   let accessSuper = membership?.accessSuper ?? false;
 
+  // This corresponds to
+  // https://github.com/AstroPlant/astroplant-api/blob/3cc10c726b1d1cbb4185193d756febe19a0c8e09/astroplant-api/src/authorization/mod.rs#L39-L81
   return {
     resetPassword: accessSuper,
     editDetails: accessConfigure,
