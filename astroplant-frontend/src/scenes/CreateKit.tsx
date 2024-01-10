cf24068ccf058c3387c899501f0ba3c70640a7a9
Thomas Churchman <thomas@kepow.org>
2024-01-10 17:22:36 +0100

    Improve kit creation page

--- astroplant-frontend/src/scenes/CreateKit.tsx
+++ astroplant-frontend/src/scenes/CreateKit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import {
@@ -19,6 +19,8 @@ import { Response, api, schemas } from "~/api";
 import { useAppDispatch } from "~/hooks";
 import { kitCreated } from "~/modules/me/actions";
 import ApiForm from "~/Components/ApiForm";
+
+import commonStyle from "~/Common.module.css";
 
 const CreateKitForm = ApiForm<
   any,
@@ -103,93 +105,101 @@ function CreateKit() {
 
   return (
     <>
-      <HeadTitle main={t("createKit.header")} />
-      <Container text style={{ marginTop: "1em" }}>
-        <Segment padded>
-          {done ? (
-            <>
-              <Header size="huge" icon textAlign="center">
-                <Transition animation="drop" duration={450} transitionOnMount>
-                  <Icon name="check" circular />
-                </Transition>
-                <Header.Content>Success!</Header.Content>
-              </Header>
-              <Container textAlign="center">
-                <p>
-                  Your kit has been created. A password has been generated as
-                  well.
-                </p>
-                <p>
-                  <strong>This password will not be shown again.</strong>
-                </p>
-                <p>
-                  If you lose it,{" "}
-                  <strong>you can always generate a new password</strong> from
-                  the kit's configuration screen.
-                </p>
-                <p>
-                  This password and the kit's serial number should be added to{" "}
-                  <code>kit_config.toml</code> file in the home directory of the
-                  kit. <code>/home/pi</code>
-                  <br />
-                  Read more about setting up the password{" "}
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://docs.astroplant.io/astroplant-kit-setup/astroplant-software/quick-software-setup#installing-the-bare-image"
-                  >
-                    here
-                  </a>
-                </p>
-                <Card color="orange" centered raised fluid>
-                  <Card.Content>
-                    <Card.Header>Kit serial</Card.Header>
-                    <Card.Description>
-                      <Input
-                        fluid
-                        icon={<Icon name="lock" inverted circular link />}
-                        value={result?.kitSerial ?? ""}
-                        onClick={(ev: any) => ev.target.select()}
-                        readOnly
-                      />
-                    </Card.Description>
-                  </Card.Content>
-                </Card>
-                <Card color="orange" centered raised fluid>
-                  <Card.Content>
-                    <Card.Header>Kit password</Card.Header>
-                    <Card.Description>
-                      <Input
-                        fluid
-                        icon={<Icon name="lock" inverted circular link />}
-                        value={result?.password ?? ""}
-                        onClick={(ev: any) => ev.target.select()}
-                        readOnly
-                      />
-                    </Card.Description>
-                  </Card.Content>
-                </Card>
-                <p>
-                  <Link to={`/kit/${result?.kitSerial ?? ""}`}>Click here</Link>{" "}
-                  to go to its dashboard.
-                </p>
-              </Container>
-            </>
-          ) : (
-            <>
-              <CreateKitForm
-                idPrefix="createKitForm"
-                schema={schema}
-                uiSchema={uiSchema}
-                transform={transform}
-                send={send}
-                onResponse={onResponse}
-                submitLabel={t("createKit.create")}
-              />
-            </>
-          )}
-        </Segment>
-      </Container>
+      <article
+        className={commonStyle.containerRegular}
+        style={{ marginTop: "2rem", marginBottom: "2rem" }}
+      >
+        <h2>{t("createKit.header")}</h2>
+        {done ? (
+          <>
+            <Header size="huge" icon textAlign="center">
+              <Transition animation="drop" duration={450} transitionOnMount>
+                <Icon name="check" circular />
+              </Transition>
+              <Header.Content>Success!</Header.Content>
+            </Header>
+            <Container textAlign="center">
+              <p>
+                Your kit has been created. A password has been generated as
+                well.
+              </p>
+              <p>
+                <strong>This password will not be shown again.</strong>
+              </p>
+              <p>
+                If you lose it,{" "}
+                <strong>you can always generate a new password</strong> from the
+                kit's configuration screen.
+              </p>
+              <p>
+                This password and the kit's serial number should be added to{" "}
+                <code>kit_config.toml</code> file in the home directory of the
+                kit. <code>/home/pi</code>
+                <br />
+                Read more about setting up the password{" "}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://docs.astroplant.io/astroplant-kit-setup/astroplant-software/quick-software-setup#installing-the-bare-image"
+                >
+                  here
+                </a>
+                .
+              </p>
+              <Card color="orange" centered raised fluid>
+                <Card.Content>
+                  <Card.Header>Kit serial</Card.Header>
+                  <Card.Description>
+                    <Input
+                      fluid
+                      icon={<Icon name="lock" inverted circular link />}
+                      value={result?.kitSerial ?? ""}
+                      onClick={(ev: any) => ev.target.select()}
+                      readOnly
+                    />
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+              <Card color="orange" centered raised fluid>
+                <Card.Content>
+                  <Card.Header>Kit password</Card.Header>
+                  <Card.Description>
+                    <Input
+                      fluid
+                      icon={<Icon name="lock" inverted circular link />}
+                      value={result?.password ?? ""}
+                      onClick={(ev: any) => ev.target.select()}
+                      readOnly
+                    />
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+              <p>
+                <Link to={`/kit/${result?.kitSerial ?? ""}`}>Click here</Link>{" "}
+                to go to the dashboard of the kit you just created.
+              </p>
+            </Container>
+          </>
+        ) : (
+          <>
+            <p>
+              Once the kit has been created, you can create configurations for
+              it. To use the kit on your device, copy the credentials that will
+              be generated to the device (a kit serial and a password). You
+              normally create one kit per physical device.
+            </p>
+            <CreateKitForm
+              idPrefix="createKitForm"
+              schema={schema}
+              uiSchema={uiSchema}
+              transform={transform}
+              send={send}
+              onResponse={onResponse}
+              submitLabel={t("createKit.create")}
+            />
+          </>
+        )}
+      </article>
     </>
   );
 }
