28ab8bd22bfa5de01e5d90deae2411c57ac4a9b7
Thomas Churchman <thomas@kepow.org>
2024-01-16 22:31:14 +0100

    feat: add debounce utility hook

--- astroplant-frontend/src/hooks.ts
+++ astroplant-frontend/src/hooks.ts
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
+
 import type { RootState, AppDispatch } from "./store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+/**
+ * Debounce a value by a specific delay.
+ *
+ * `delayMs` the debounce delay in milliseconds.
+ */
+export function useDebounce<T>(value: T, delayMs: number): T {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebounced(value);
+    }, delayMs);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delayMs]);
+
+  return debounced;
+}
