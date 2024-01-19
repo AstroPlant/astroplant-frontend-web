3a10e9d69ddf72cf5b6318ea4ff359d09f12ba86
Thomas Churchman <thomas@kepow.org>
2024-01-19 16:14:43 +0100

    refactor: use in-repo container in NotFound and simplify style

--- astroplant-frontend/src/pages/NotFound.tsx
+++ astroplant-frontend/src/pages/NotFound.tsx
@@ -1,22 +1,23 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Header, Segment } from "semantic-ui-react";
 
 import HeadTitle from "../Components/HeadTitle";
+
+import commonStyle from "~/Common.module.css";
 
 export default function TermsAndConditions() {
   return (
     <>
       <HeadTitle main="Oh no!" />
-      <Container text style={{ marginTop: "1em" }}>
-        <Segment>
-          <Header>That page was not found</Header>
-          <p>The page you browsed to could not be found.</p>
-          <p>
-            Consider going to the <Link to="/home">home page</Link>.
-          </p>
-        </Segment>
-      </Container>
+      <article
+        className={commonStyle.containerWide}
+        style={{ marginTop: "1rem" }}
+      >
+        <h2>That page was not found</h2>
+        <p>The page you browsed to could not be found.</p>
+        <p>
+          Consider going to the <Link to="/home">home page</Link>.
+        </p>
+      </article>
     </>
   );
 }
